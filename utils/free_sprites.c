0b5ad5eeebc1fc0b7d5235cf924f812aa202c3b9
Victor De Souza Valente <victde-s@c1r8p3.42sp.org.br>
2025-12-03 21:50:51 -0300

    factoring: function to free and destroy the xpms, used for errors and to free in the end of the main

---
+++ utils/free_sprites.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_sprites.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 21:43:09 by victde-s          #+#    #+#             */
+/*   Updated: 2025/12/03 21:47:16 by victde-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <so_long.h>
+
+void	free_sprites(t_game *game)
+{
+	if (game->img.wall)
+		mlx_destroy_image(game->mlx.mlx, game->img.wall);
+	if (game->img.floor)
+		mlx_destroy_image(game->mlx.mlx, game->img.floor);
+	if (game->img.player)
+		mlx_destroy_image(game->mlx.mlx, game->img.player);
+	if (game->img.collectible)
+		mlx_destroy_image(game->mlx.mlx, game->img.collectible);
+	if (game->img.exit)
+		mlx_destroy_image(game->mlx.mlx, game->img.exit);
+		
+}
