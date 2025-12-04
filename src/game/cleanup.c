ca231a66a87277cf542d6abc4d59716ada6126f8
Victor De Souza Valente <victde-s@c1r8p3.42sp.org.br>
2025-12-04 02:09:01 -0300

    factoring: game cleanup free functions

---
+++ src/game/cleanup.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/04 01:32:37 by victde-s          #+#    #+#             */
+/*   Updated: 2025/12/04 02:06:37 by victde-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <so_long.h>
+
+static void	free_map(char **grid)
+{
+	int	i;
+
+	if (!grid)
+		return ;
+	i = 0;
+	while (grid[i])
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
+void	cleanup_game(t_game *game)
+{
+	free_sprites(game);
+	if (game->mlx.win)
+		mlx_destroy_window(game->mlx.mlx, game->mlx.win);
+	if (game->map.grid)
+		free_map(game->map.grid);
+	if (game->mlx.mlx)
+	{
+		mlx_destroy_display(game->mlx.mlx);
+		free(game->mlx.mlx);
+	}
+}
+void	exit_game(t_game *game, char *message)
+{
+	if (message)
+		ft_printf("%s\n", message);
+	cleanup_game(game);
+	exit(0);
+}
--- src/game/game_init.c
+++ src/game/game_init.c
@@ -6,65 +6,17 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 17:36:42 by victde-s          #+#    #+#             */
-/*   Updated: 2025/12/01 18:32:37 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/12/04 02:01:10 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <so_long.h>
 
-static int	game_find_player(t_game *game)
-{
-	int	y;
-	int	x;
-
-	y = 0;
-	while (y < game->map.height)
-	{
-		x = 0;
-		while (x < game->map.width)
-		{
-			if (game->map.grid[y][x] == PLAYER)
-			{
-				game->player.y = y;
-				game->player.x = x;
-				return (1);
-			}
-			x++;
-		}
-		y++;
-	}
-	return (put_error("Player not found!"));
-	}
-static int	game_load_sprites_map(t_game *game)
-{
-	
-}
-static int	game_load_sprites_player(t_game *game)
-{
-	
-}
 int	game_init(t_game *game)
 {
-	if (!game_find_player(game))
-		return (0);
+	ft_memset(game, 0, sizeof(t_game));
 	game->mlx.mlx = mlx_init();
 	if (!game->mlx.mlx)
-		return (message_error("Failed to init MLX!"));
-	game->mlx.win = mlx_new_window(game->mlx.mlx,
-			game->map.width * SPRITE_SIZE,
-			game->map.height * SPRITE_SIZE, "so long");
-	if (!game->mlx.win)
-	{
-		cleanup_game(game);
-		return (message_error("Failed to create window!"));
-	}
-	if (!game_load_sprites_map(game) || !game_load_sprites_player(game))
-	{
-		cleanup_game(game);
-		return (0);
-	}
-	game->map.total_c = map_count_element(game->map.grid, COLLECT);
-	game->player.direction = PLAYER_RIGHT_SPRITE;
-	game->player.steps = 0;
+		return (put_error("Failed to init MLX"));
 	return (1);
 }
