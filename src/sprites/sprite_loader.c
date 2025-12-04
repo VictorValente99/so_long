a0abbf94d90c9543d0ecea332b7eda64bd894473
Victor De Souza Valente <victde-s@c1r8p3.42sp.org.br>
2025-12-04 02:13:47 -0300

    factoring: charging images and saving on struct img, for later render

--- src/sprites/sprite_loader.c
+++ src/sprites/sprite_loader.c
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 21:20:20 by victde-s          #+#    #+#             */
-/*   Updated: 2025/12/03 21:48:17 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/12/04 00:42:43 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	load_sprites(t_game *game)
 {
 	int	width;
 	int	height;
-	
+
 	game->img.wall = mlx_xpm_file_to_image(game->mlx.mlx,
 			WALL_SPRITE, &width, &height);
 	game->img.floor = mlx_xpm_file_to_image(game->mlx.mlx,
@@ -28,10 +28,10 @@ int	load_sprites(t_game *game)
 	game->img.exit = mlx_xpm_file_to_image(game->mlx.mlx,
 			EXIT_SPRITE, &width, &height);
 	if (!game->img.wall || !game->img.floor || !game->img.player
-			|| !game->img.collectible || !game->img.exit)
-			{
-				free_sprites(game);
-				return (put_error("Failed to load sprites"));
-			}
-		return (1);
+		|| !game->img.collectible || !game->img.exit)
+	{
+		free_sprites(game);
+		return (put_error("Failed to load sprites"));
+	}
+	return (1);
 }
